f095ddc6236c1ae7fa11847f122d21bdb1e14198
Jitendra <jitendra.gedam@gmail.com>
2021-01-16 20:30:41 +0530

    Initial commit
    Sample maven project!

---
+++ src/main/java/com/jit/temp/code/App.java
@@ -0,0 +1,13 @@
+package com.jit.temp.code;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
